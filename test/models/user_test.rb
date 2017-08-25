7e00e87ac39906e85957d40c27651b702b11b868
zhaoliang <zhaoliang@motie.com>
2017-08-25 17:59:27 +0800

    fix

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,10 +9,6 @@ class UserTest < ActiveSupport::TestCase
       @user = User.new(name: "xiaohong", email:"xiaohong@motie.com", password: "liang", password_confirmation: "liang")
   end
 
-  test "should be valid" do
-    assert @user.valid?
-  end
-
   test "should be present" do 
       @user.name = "   "
       assert_not @user.valid?
